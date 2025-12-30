d4164731886104c500d97d55dea16b62b9d94d4a
FazrinNugraha <nugrahafadzrin@gmail.com>
2025-12-30 21:41:30 +0700

    add(feat):route api for getCategories

--- src/services/getCourses.js
+++ src/services/getCourses.js
@@ -1,3 +1,4 @@
 import { apiInstanceAuth } from '../utils/axios';
 
 export const getCourses = async () => apiInstanceAuth.get('/courses').then(response => response.data);
+export const getCategories = async () => apiInstanceAuth.get('/categories').then(response => response.data);
