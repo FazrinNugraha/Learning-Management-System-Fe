163b7ce4d66dce1a3cad5446b8e2c5483ebff85b
FazrinNugraha <nugrahafadzrin@gmail.com>
2026-02-15 21:24:54 +0700

    integration create course content 2

--- src/pages/manager/course-content-create/index.jsx
+++ src/pages/manager/course-content-create/index.jsx
@@ -1,26 +1,42 @@
-import React, { useState } from 'react'
-import ReactQuill from 'react-quill-new'
-import 'react-quill-new/dist/quill.snow.css'
-import { useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { mutateContentSchema } from '../../../utils/zodSchema'
+import React, { useState } from "react";
+import ReactQuill from "react-quill-new";
+import "react-quill-new/dist/quill.snow.css";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { mutateContentSchema } from "../../../utils/zodSchema";
 
 export default function ManageContentCreatePage() {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+    watch,
+  } = useForm({
+    resolver: zodResolver(mutateContentSchema),
+  });
 
-  const { register, handleSubmit, setValue, formState: { errors } } = useForm({
-    resolver: zodResolver(mutateContentSchema)
-  })
+  const type = watch("type");
 
   const onSubmit = (values) => {
-    console.log('submit content form:', values)
-  }
+    console.log("submit content form:", values);
+  };
 
   return (
     <>
-      <div id="Breadcrumb" className="flex items-center gap-5 *:after:content-['/'] *:after:ml-5">
-        <span className="last-of-type:after:content-[''] last-of-type:font-semibold">Manage Course</span>
-        <span className="last-of-type:after:content-[''] last-of-type:font-semibold">Course</span>
-        <span className="last-of-type:after:content-[''] last-of-type:font-semibold">Add Content</span>
+      <div
+        id="Breadcrumb"
+        className="flex items-center gap-5 *:after:content-['/'] *:after:ml-5"
+      >
+        <span className="last-of-type:after:content-[''] last-of-type:font-semibold">
+          Manage Course
+        </span>
+        <span className="last-of-type:after:content-[''] last-of-type:font-semibold">
+          Course
+        </span>
+        <span className="last-of-type:after:content-[''] last-of-type:font-semibold">
+          Add Content
+        </span>
       </div>
 
       <header className="flex items-center justify-between gap-[30px]">
@@ -33,7 +49,9 @@ export default function ManageContentCreatePage() {
             />
           </div>
           <div>
-            <h1 className="font-extrabold text-[28px] leading-[42px]">Add Content</h1>
+            <h1 className="font-extrabold text-[28px] leading-[42px]">
+              Add Content
+            </h1>
             <p className="text-[#838C9D] mt-[1]">
               Give a best content for the course
             </p>
@@ -45,13 +63,11 @@ export default function ManageContentCreatePage() {
         className="flex flex-col w-[930px] rounded-[30px] p-[30px] gap-[30px] bg-[#F8FAFB]"
         onSubmit={handleSubmit(onSubmit)}
       >
-
         {/* TITLE */}
         <div className="flex flex-col gap-[10px]">
           <label className="font-semibold">Content Title</label>
 
           <div className="flex items-center w-full rounded-full border border-[#CFDBEF] gap-3 px-5">
-
             <img
               src="/assets/images/icons/note-favorite-black.svg"
               className="w-6 h-6"
@@ -59,7 +75,7 @@ export default function ManageContentCreatePage() {
             />
 
             <input
-              {...register('title')}
+              {...register("title")}
               type="text"
               className="appearance-none outline-none w-full py-3 font-semibold !bg-transparent"
               placeholder="Write better name for your course"
@@ -75,7 +91,6 @@ export default function ManageContentCreatePage() {
           <label className="font-semibold">Select Type</label>
 
           <div className="flex items-center w-full rounded-full border border-[#CFDBEF] gap-3 px-5">
-
             <img
               src="/assets/images/icons/crown-black.svg"
               className="w-6 h-6"
@@ -83,7 +98,7 @@ export default function ManageContentCreatePage() {
             />
 
             <select
-              {...register('type')}
+              {...register("type")}
               className="appearance-none outline-none w-full py-3 px-2 -mx-2 font-semibold !bg-transparent"
             >
               <option value="">Select content type</option>
@@ -103,57 +118,59 @@ export default function ManageContentCreatePage() {
         </div>
 
         {/* YOUTUBE */}
-        <div className="flex flex-col gap-[10px]">
-          <label className="font-semibold">Youtube Video ID</label>
+        {type === "video" && (
+          <div className="flex flex-col gap-[10px]">
+            <label className="font-semibold">Youtube Video ID</label>
 
-          <div className="flex items-center w-full rounded-full border border-[#CFDBEF] gap-3 px-5">
+            <div className="flex items-center w-full rounded-full border border-[#CFDBEF] gap-3 px-5">
+              <img
+                src="/assets/images/icons/bill-black.svg"
+                className="w-6 h-6"
+                alt="icon"
+              />
 
-            <img
-              src="/assets/images/icons/bill-black.svg"
-              className="w-6 h-6"
-              alt="icon"
-            />
-
-            <input
-              {...register('youtubeId')}
-              type="text"
-              className="appearance-none outline-none w-full py-3 font-semibold !bg-transparent"
-              placeholder="Write tagline for better copy"
-            />
+              <input
+                {...register("youtubeId")}
+                type="text"
+                className="appearance-none outline-none w-full py-3 font-semibold !bg-transparent"
+                placeholder="Write tagline for better copy"
+              />
+            </div>
+            <span className="error-message text-[#FF435A]">
+              {errors?.youtubeId?.message}
+            </span>
           </div>
-          <span className="error-message text-[#FF435A]">
-            {errors?.youtubeId?.message}
-          </span>
-        </div>
+        )}
 
         {/* CONTENT */}
-        <div className="flex flex-col gap-[10px]">
-          <label className="font-semibold">Content Text</label>
+        {type === "text" && (
+          <div className="flex flex-col gap-[10px]">
+            <label className="font-semibold">Content Text</label>
 
-          <ReactQuill
-            theme="snow"
-            placeholder="Tulis konten materi di sini..."
-            modules={{
-              toolbar: [
-                [{ 'header': [1, 2, 3, false] }],
-                ['bold', 'italic'],
-                ['link'],
-                [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-                ['clean']
-              ]
-            }}
-            onChange={(value) => {
-              setValue('text', value)
-            }}
-          />
-          <span className="error-message text-[#FF435A]">
-            {errors?.text?.message}
-          </span>
-        </div>
+            <ReactQuill
+              theme="snow"
+              placeholder="Tulis konten materi di sini..."
+              modules={{
+                toolbar: [
+                  [{ header: [1, 2, 3, false] }],
+                  ["bold", "italic"],
+                  ["link"],
+                  [{ list: "ordered" }, { list: "bullet" }],
+                  ["clean"],
+                ],
+              }}
+              onChange={(value) => {
+                setValue("text", value);
+              }}
+            />
+            <span className="error-message text-[#FF435A]">
+              {errors?.text?.message}
+            </span>
+          </div>
+        )}
 
         {/* BUTTON */}
         <div className="flex items-center gap-[14px]">
-
           <button
             type="button"
             className="w-full rounded-full border p-[14px_20px] font-semibold"
@@ -167,10 +184,8 @@ export default function ManageContentCreatePage() {
           >
             Add Content Now
           </button>
-
         </div>
-
       </form>
     </>
-  )
+  );
 }
